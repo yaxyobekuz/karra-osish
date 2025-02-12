3226e3c85b80f3751a7928b0027f3c397b6ece97
yaxyobekb <pubgn9642@gmail.com>
2025-02-12 18:35:12 +0500

    refactor Purposes component to use structured data and add background images with icons

--- src/components/Purposes.jsx
+++ src/components/Purposes.jsx
@@ -6,7 +6,10 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 // Data
-import information from "../data/information";
+import purposes from "../data/purposes";
+
+// Images
+import polygonIcon from "../assets/images/icons/polygon.svg";
 
 const Purposes = () => {
   const listRef = useRef([]);
@@ -30,21 +33,34 @@ const Purposes = () => {
 
   return (
     <ol className="grid grid-cols-3 gap-5">
-      {information.map((info, index) => (
+      {purposes.map(({ title, bg, color }, index) => (
         <li
           key={index}
+          style={{ background: bg, color }}
           ref={(el) => (listRef.current[index] = el)}
-          className="flex flex-col justify-between gap-5 min-h-52 bg-[#027FCC] p-8 rounded-3xl"
+          className="flex flex-col justify-between gap-5 relative overflow-hidden min-h-52 !bg-cover !bg-no-repeat p-8 rounded-3xl"
         >
-          <span className="text-3xl font-semibold text-white">
+          {/* Index */}
+          <span className="text-3xl font-semibold text-whit">
             0{index + 1}.
           </span>
 
-          {/* Details */}
-          <div className="space-y-3.5">
-            {/* Item title */}
-            <h3 className="font-semibold text-lg text-white">{info}</h3>
-          </div>
+          {/* Item title */}
+          <h3 className="z-10 font-semibold text-lg text-whit">{title}</h3>
+
+          {/* Polygon */}
+          <img
+            width={192}
+            height={192}
+            src={polygonIcon}
+            alt="Polygon icon"
+            className="absolute size-48 opacity-30 -top-14 -right-14"
+          />
+
+          {/* Text overlay */}
+          {bg.startsWith("url") && (
+            <div className="absolute inset-x-0 bottom-0 h-40 bg-gradient-to-b from-transparent to-black" />
+          )}
         </li>
       ))}
     </ol>
