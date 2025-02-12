42c87e80694978549808eda782fb9d52a3d04441
yaxyobekb <pubgn9642@gmail.com>
2025-02-12 18:34:57 +0500

    update information.js with new strategic growth insights

--- src/data/information.js
+++ src/data/information.js
@@ -1,8 +1,7 @@
 export default [
-  "Tizimli ravishda raqobatchilardan ustunlikka erishish.",
-  "Biznesni tizimlashtirish: xodimlarni ishga olish, sotuv, marketing.",
-  "Qanday qilib jamoani samarali boshqarish va eng kuchli jamoani yig'ish.",
-  "O'zingiz uchun shaxsiy vaqt ajratish va operatsion tizimdan chiqish.",
-  "O'sish nuqtasini topib, sof foydani oshirish.",
-  "Mijozlar oqimini bir necha barobar oshirish.",
+  "Masshtablash uchun bosqichma-bosqich reja.",
+  "Tizimli o'sish uchun ko'rsatmalar - moliyadan jamoagacha.",
+  "Milliard rublik loyihalarni amalga oshirishda yordam bergan harakatlar.",
+  "Biznesga ziyon yetkazmagan holda masshtablashning aniq ko'rsatmalari.",
+  "Qanday qilib mijozlarni ko'paytirish.",
 ];
