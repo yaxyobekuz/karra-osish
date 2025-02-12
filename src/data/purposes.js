2d7900fdf8c464f9d9ddb4e19415baf687d13ce8
yaxyobekb <pubgn9642@gmail.com>
2025-02-12 18:35:03 +0500

    refactor purposes.js to use objects for structured data and add background images

--- src/data/purposes.js
+++ src/data/purposes.js
@@ -1,7 +1,37 @@
+import cupImg from "../assets/images/others/cup.png";
+
 export default [
-  "Tizimli ravishda raqobatchilardan ustunlikka erishish.",
-  "Biznesni tizimlashtirish: xodimlarni ishga olish, sotuv, marketing.",
-  "Qanday qilib jamoani samarali boshqarish va eng kuchli jamoani yig'ish.",
-  "O'zingiz uchun shaxsiy vaqt ajratish va operatsion tizimdan chiqish.",
-  "O'sish nuqtasini topib, sof foydani oshirish.",
+  {
+    title: "Tizimli ravishda raqobatchilardan ustunlikka erishish.",
+    bg: "url(https://w0.peakpx.com/wallpaper/658/542/HD-wallpaper-businesses-take-advantage-of-real-time-cyber-intelligence-partner-content.jpg)",
+    color: "white",
+  },
+  {
+    title:
+      "Biznesni tizimlashtirish: xodimlarni ishga olish, sotuv, marketing.",
+    bg: "#028DE3",
+    color: "#FFFFFF",
+  },
+  {
+    title:
+      "Qanday qilib jamoani samarali boshqarish va eng kuchli jamoani yig'ish.",
+      bg: "url(https://www.gmevents.ae/wp-content/uploads/2019/04/228109899-business-meeting-wallpaper.jpg)",
+      color: "white",
+    },
+  {
+    title:
+      "O'zingiz uchun shaxsiy vaqt ajratish va operatsion tizimdan chiqish.",
+    bg: "#E2FFC8",
+    color: "#292a31",
+  },
+  {
+    title: "O'sish nuqtasini topib, sof foydani oshirish.",
+    bg: "url(https://t4.ftcdn.net/jpg/01/34/25/15/360_F_134251559_Kiq7uliJiXSFUZAM6upX7rdJWL5Z52jF.jpg)",
+    color: "white",
+  },
+  {
+    title: "Mijozlar oqimini bir necha barobar oshirish.",
+    bg: "#292a31",
+    color: "white",
+  },
 ];
