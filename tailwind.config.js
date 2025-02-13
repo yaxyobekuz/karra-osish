b13c41f6d10ab936dd58c5cdeb8348191934c735
yaxyobekb <pubgn9642@gmail.com>
2025-02-13 17:54:48 +0500

    update CSS for smoother scrolling and adjust container padding; add responsive breakpoints in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,15 @@ export default {
         Manrope: ["Manrope", "sans-serif"],
       },
     },
+
+    screens:{
+      xs: "425px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    }
   },
   plugins: [],
 };
