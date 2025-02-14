edecb8dfd9bfe57d3021881e396c96a5fa2fa09c
yaxyobekb <pubgn9642@gmail.com>
2025-02-15 00:10:57 +0500

    Refactor CSS for improved dark mode support and adjust button styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "media",
   theme: {
     extend: {
       colors: {
         primary: "#028DE3",
+        light: "#171717",
         dark: "#292a31",
-        gray: {
-          light: "#F5F7F9",
-        },
+        gray: { light: "#F5F7F9" },
       },
 
       fontFamily: {
