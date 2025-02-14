3f1b56685c85ad0e177a5127762def5eb1483e65
yaxyobekb <pubgn9642@gmail.com>
2025-02-14 16:12:40 +0500

    Add play button overlay to comments section and include play icon

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
+
+// Data
+import comments from "../data/comments";
+
 // Swiper
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Data
-import comments from "../data/comments";
+// Images
+import playIcon from "../assets/images/icons/play.svg";
 
 const Comments = () => {
+  const [activeComment, setActiveComment] = useState(null);
+
   return (
     <div className="container">
       <Swiper
@@ -30,18 +37,38 @@ const Comments = () => {
           nextEl: ".comments-swiper-btn-next",
         }}
       >
-        {comments.map((url, index) => (
-          <SwiperSlide key={index} className="h-full bg-white rounded-2xl">
-            <iframe
-              src={url}
-              allowFullScreen
-              title="YouTube video player"
-              className="size-full rounded-2xl"
-              referrerPolicy="no-referrer-when-downgrade"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            />
-          </SwiperSlide>
-        ))}
+        {comments.map((url, index) => {
+          return (
+            <SwiperSlide
+              key={index}
+              className="relative h-full bg-white rounded-2xl"
+            >
+              {activeComment !== index ? (
+                <button
+                  onClick={() => setActiveComment(index)}
+                  className="flex items-center justify-center absolute top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2 size-11 bg-primary rounded-full"
+                >
+                  <img
+                    width={20}
+                    height={20}
+                    src={playIcon}
+                    alt="Play icon"
+                    className="size-5 translate-x-0.5"
+                  />
+                </button>
+              ) : (
+                <iframe
+                  src={url}
+                  allowFullScreen
+                  title="YouTube video player"
+                  className="size-full rounded-2xl"
+                  referrerPolicy="no-referrer-when-downgrade"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                />
+              )}
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
